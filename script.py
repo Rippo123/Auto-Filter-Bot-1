592e91b514a4404fa508142599c4f763eccfeeaa
joefreddiecheeran <70432335+joefreddiecheeran@users.noreply.github.com>
2021-04-18 22:19:06 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,65 @@
+class script(object):
+
+
+    START_MSG = """ <b>Hi {}
+
+You can call this as an Auto Filter Bot if you like :D
+
+Bot gives button link to files in connected channels on query !
+
+No need to add filters for your files or movies from now on!
+
+For more click <i>help</i></b>"""
+
+
+    HELP_MSG = """<b>How to use the bot??</b>
+
+<i>
+* Add bot to your group with admin rights.
+
+* Add bot to channels which you want to link with <b>all admin rights</b>!
+</i>
+
+
+<b>Bot Commands - Works in Group only</b>
+(You need to be a Auth User in order to use these commands)
+
+* <code>/add channelid</code>  -  Links channel to your group.
+or
+* <code>/add @channelusername</code> - Links channel to your group.
+
+<i>NOTE : You can get your channel ID from @ChannelidHEXbot </i>
+
+
+* <code>/del channelid</code>  -  Delinks channel from group
+or
+* <code>/del @channelusername</code>  -  Delinks channel from group
+
+<i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
+
+
+* <code>/delall</code>  -  Removes all connected channels and filters from group!
+
+<i>Note : Dont add command delete bots in group! Otherwise, delall command wont work. Post some content in your channel before giving the connect command to the bot.</i>
+
+
+* <code>/filterstats</code>  -  Check connected channels and number of filters.
+
+
+
+No need add each filter again!
+Bot will automatically search for your files and give links to that!
+
+
+<b>© @alphantime</b>"""
+
+
+    ABOUT_MSG = """⭕️<b>My Name : ALPHA - Auto Filter Bot </b>
+    
+⭕️<b>Creater :</b> @alphantime
+
+⭕️<b>Language :</b> <code>Python3</code>
+
+⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+
+"""
